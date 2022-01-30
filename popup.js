cd8eea08806f28f0a55635524370a4d8d69de1eb
Alicja <alicja.x.lukaszewska@gmail.com>
2022-01-30 22:33:06 +0100

    Ability to save rearenged tags

---
+++ popup.js
@@ -0,0 +1,202 @@
+const listTypes = ["relationship", "character", "freeform"];
+
+const saveSuccess = (info) => {
+  console.log("saved successfully!", info);
+};
+
+const saveEdits = () => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        {
+          from: "popup",
+          subject: "removeTags",
+        },
+        saveSuccess
+      );
+    }
+  );
+
+  listTypes.forEach((type) => {
+    const list = document.getElementById(`${type}-list`);
+    const listItems = list.getElementsByTagName("span");
+    if (!listItems) return;
+
+    [...listItems].forEach((item) => {
+      chrome.tabs.query(
+        {
+          active: true,
+          currentWindow: true,
+        },
+        (tabs) => {
+          chrome.tabs.sendMessage(
+            tabs[0].id,
+            {
+              from: "popup",
+              subject: "updateInfo",
+              tag: item.innerText,
+              listType: type,
+            },
+            saveSuccess
+          );
+        }
+      );
+    });
+  });
+
+  setTimeout(() => {
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true,
+      },
+      (tabs) => {
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          {
+            from: "popup",
+            subject: "finishedUpdate",
+          },
+          saveSuccess
+        );
+      }
+    );
+  }, 1500);
+};
+
+const manageNewTagInputs = () => {
+  listTypes.forEach((type) => {
+    const list = document.getElementById(`${type}-list`);
+    const input = document.getElementById(`${type}-input`);
+    input.addEventListener("keyup", (e) => {
+      if (e.code === "Comma" || e.code === "Enter") {
+        const valueWithoutComma = input.value.trim().replace(/[,]+/, "");
+        const tagElement = generateListElement(valueWithoutComma);
+        list.appendChild(tagElement);
+        input.value = "";
+      }
+    });
+  });
+};
+
+const activateSortList = (target) => {
+  const listItems = target.getElementsByTagName("li");
+  let current = null;
+
+  if (!listItems) return;
+
+  [...listItems].forEach((item) => {
+    item.ondragover = (evt) => {
+      evt.preventDefault();
+    };
+
+    item.ondragstart = () => {
+      current = item;
+      [...listItems].forEach((listItem) => {
+        if (listItem.id !== current.id) {
+          listItem.classList.add("hint");
+        }
+      });
+    };
+
+    item.ondragenter = () => {
+      if (item.id !== current.id) {
+        item.classList.add("active");
+      }
+    };
+
+    item.ondragleave = () => {
+      item.classList.remove("active");
+    };
+
+    item.ondragend = () => {
+      [...listItems].forEach((listItem) => {
+        listItem.classList.remove("hint");
+        listItem.classList.remove("active");
+        item.classList.add("ready");
+        setTimeout(() => {
+          item.classList.remove("ready");
+        }, 1000);
+      });
+    };
+
+    item.ondrop = (evt) => {
+      evt.preventDefault();
+      if (item.id !== current.id) {
+        const currentPosition = [...listItems].indexOf(current);
+        const droppedPosition = [...listItems].indexOf(item);
+        if (currentPosition < droppedPosition) {
+          item.parentNode.insertBefore(current, item.nextSibling);
+        } else {
+          item.parentNode.insertBefore(current, item);
+        }
+      }
+    };
+  });
+};
+
+const addSortingToListElement = () => {
+  listTypes.forEach((type) => {
+    activateSortList(document.getElementById(`${type}-list`));
+  });
+};
+
+const removeTag = (e) => {
+  const listElement = e.srcElement.parentNode;
+  listElement.remove();
+};
+
+const generateListElement = (tag) => {
+  const tagElement = document.createElement("li");
+  const tagSpanElement = document.createElement("span");
+  tagSpanElement.innerHTML = tag.trim();
+  const removeBtn = document.createElement("button");
+  removeBtn.className = "remove-btn";
+  removeBtn.innerHTML = "&#10005;";
+  removeBtn.title = "Delete tag";
+  tagElement.id = tag.trim();
+  tagElement.class = "list-element";
+  tagElement.draggable = true;
+  tagElement.appendChild(tagSpanElement);
+  tagElement.appendChild(removeBtn);
+  removeBtn.addEventListener("click", removeTag);
+  return tagElement;
+};
+
+const generateListElements = (info) => {
+  if (!info) return;
+  Object.keys(info).forEach((tagType) => {
+    const list = document.getElementById(`${tagType}-list`);
+    const tags = info[tagType].split(",");
+    tags.forEach((tag) => {
+      const tagElement = generateListElement(tag);
+      list.appendChild(tagElement);
+    });
+  });
+  addSortingToListElement();
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(
+        tabs[0].id,
+        { from: "popup", subject: "DOMInfo" },
+        generateListElements
+      );
+    }
+  );
+  const saveBtn = document.getElementById("save-btn");
+  if (saveBtn) saveBtn.addEventListener("click", saveEdits);
+
+  manageNewTagInputs();
+});
---
+++ script.js
@@ -0,0 +1,119 @@
+// Inform the background page that
+// this tab should have a page-action.
+chrome.runtime.sendMessage({
+  from: "content",
+  subject: "showPageAction",
+});
+
+const dummyIframeName = "dummy-iframe";
+
+const manageDummyIframe = () => {
+  //prevent from creating more than one iframe
+  if (document.querySelector(`iframe#${dummyIframeName}`)) return;
+
+  const iframe = document.createElement("iframe");
+  iframe.name = dummyIframeName;
+  iframe.id = dummyIframeName;
+  iframe.style.display = "none";
+  const body = document.querySelector("body");
+  body.appendChild(iframe);
+};
+
+const manageTargetToForm = (add = false) => {
+  const form = document.getElementById("work-form");
+  if (add) form.target = dummyIframeName;
+  else form.removeAttribute("target");
+};
+
+const clickPostBtn = () => {
+  const postBtn = document.getElementsByName("post_button")[0];
+  if (postBtn) {
+    postBtn.removeAttribute("data-disable-with");
+    postBtn.click();
+  }
+};
+
+const saveWithoutReload = () => {
+  manageTargetToForm(true);
+  clickPostBtn();
+  manageTargetToForm();
+};
+
+const removeCurrentTags = () => {
+  const listTypes = ["relationship", "character", "freeform"];
+
+  listTypes.forEach((listType) => {
+    const tagsOfListParent = document.querySelector(`dd.${listType}`);
+    const removeBtns = tagsOfListParent.querySelectorAll("span.delete a");
+    if (removeBtns && removeBtns.length) {
+      [...removeBtns].forEach((btn) => {
+        btn.click();
+      });
+    }
+  });
+  manageDummyIframe();
+  saveWithoutReload();
+};
+
+const addTagsFromPlugin = (msg) => {
+  const listInput = document.getElementById(
+    `work_${msg.listType}_autocomplete`
+  );
+  listInput.value = msg.tag;
+  listInput.focus();
+  listInput.dispatchEvent(new KeyboardEvent("keydown", { key: "Enter" }));
+  listInput.blur();
+};
+
+// Listen for messages from the popup.
+chrome.runtime.onMessage.addListener((msg, sender, response) => {
+  // First, validate the message's structure.
+  if (msg.from === "popup" && msg.subject === "DOMInfo") {
+    const domInfo = {
+      relationship: document.getElementById("work_relationship").value,
+      character: document.getElementById("work_character").value,
+      freeform: document.getElementById("work_freeform").value,
+    };
+
+    // Directly respond to the sender (popup),
+    // through the specified callback.
+    response(domInfo);
+  }
+
+  if (msg.from === "popup" && msg.subject === "removeTags") {
+    removeCurrentTags();
+
+    const res = "remove";
+    response(res);
+  }
+
+  if (msg.from === "popup" && msg.subject === "updateInfo") {
+    addTagsFromPlugin(msg);
+
+    const res = "add";
+    response(res);
+  }
+  if (msg.from === "popup" && msg.subject === "finishedUpdate") {
+    saveWithoutReload();
+
+    const res = "finished";
+    response(res);
+  }
+});
+
+function generateTagList(id) {
+  const tagsInput = document.getElementById(id);
+
+  chrome.runtime.sendMessage({
+    from: "content",
+    subject: tagsInput.value,
+  });
+}
+
+function getTags() {
+  const inputsIds = ["work_relationship", "work_character", "work_freeform"];
+
+  inputsIds.forEach((id) => generateTagList(id));
+}
+
+// getTags();
